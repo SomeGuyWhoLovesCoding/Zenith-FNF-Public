4fde2fee2eee76e6abddb465e54ffa51b823472f
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-05-29 12:37:52 -0400

    Update NoteBase.hx

--- source/zenith/objects/NoteBase.hx
+++ source/zenith/objects/NoteBase.hx
@@ -24,10 +24,4 @@ class NoteBase extends StaticSprite
 
 	static public var colorArray:Array<Int> = [0xffc941d5, 0xff00ffff, 0xff0ffb3e, 0xfffa3e3e];
 	static public var angleArray:Array<Float> = [0.0, -90.0, 90.0, 180.0];
-
-	public function new():Void
-	{
-		super();
-		active = false;
-	}
 }
