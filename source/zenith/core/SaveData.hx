4acfffc602e6c236096c710c8f425395e3ef2f45
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-05-31 21:04:43 -0400

    Rewrite custom savedata system
    This is untested but should work fine

--- source/zenith/core/SaveData.hx
+++ source/zenith/core/SaveData.hx
@@ -1,11 +1,12 @@
 package zenith.core;
 
 import lime.ui.KeyCode;
-import haxe.Json;
+import sys.io.File;
+import haxe.ds.StringMap;
 
 class SaveData
 {
-	static public var contents:SaveFile = {
+	static public var contents(default, null):SaveFile = {
 		preferences: {
 			downScroll: false,
 			smoothHealth: true,
@@ -37,26 +38,64 @@ class SaveData
 			RESET: KeyCode.DELETE,
 			CONTROL: KeyCode.LEFT_CTRL
 		},
-		extraData: null
+		customData: new StringMap<StringMap<Dynamic>>()
 	};
 
 	static public function reloadSave():Void
 	{
 		if (sys.FileSystem.exists('savedata.sav'))
 		{
-			var contentStr:String = sys.io.File.getContent('savedata.sav');
+			var contentStr:String = File.getContent('savedata.sav');
 			if (contentStr != '')
-				contents = (Json.parse(contentStr) : SaveFile);
+				contents = (haxe.Unserializer.run(contentStr) : SaveFile);
 		}
 		else
-			sys.io.File.saveContent('savedata.sav', '');
-		// WIP
+		{
+			File.saveContent('savedata.sav', '');
+		}
 	}
 
 	static public function saveContent():Void
 	{
-		sys.io.File.saveContent('savedata.sav', Json.stringify(contents, '\t'));
+		File.saveContent('savedata.sav', haxe.Serializer.run(contents : SaveFile));
 	}
+
+	inline static public function createCustomSave(sav:String):Void
+	{
+		if (contents.customData[sav] == null)
+		{
+			contents.customData[sav] = new StringMap<Dynamic>();
+		}
+	}
+
+	inline static public function changeCustomSaveName(sav:String, name:String):Void
+	{
+		if (contents.customData[name] == null)
+		{
+			contents.customData[name] = contents.customData[sav].copy();
+			deleteCustomSave(sav);
+		}
+	}
+
+	inline static public function setDataFromCustomSave(sav:String, data:String, content:Dynamic):Void
+	{
+		if ((s = contents.customData[sav]) != null)
+		{
+			s[data] = content;
+		}
+	}
+
+	inline static public function deleteCustomSave(sav:String):Void
+	{
+		if ((s = contents.customData[sav]) != null)
+		{
+			s.clear();
+			s = null;
+			contents.customData.remove(sav);
+		}
+	}
+
+	var s(default, null):StringMap<Dynamic>;
 }
 
 typedef PreferencesData =
@@ -96,5 +135,5 @@ typedef SaveFile =
 	var preferences:PreferencesData;
 	var graphics:GraphicsData;
 	var controls:ControlsData;
-	var extraData:Dynamic;
+	var customData:StringMap<StringMap<Dynamic>>;
 }
