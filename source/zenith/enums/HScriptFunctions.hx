c3cdd112c0e860d83313b331303dc5d9da6ede7a
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-06-03 08:32:15 -0400

    Update HScriptFunctions.hx

--- source/zenith/enums/HScriptFunctions.hx
+++ source/zenith/enums/HScriptFunctions.hx
@@ -1,42 +1,42 @@
 package zenith.enums;
 
-enum abstract HScriptFunctions(String)
+class HScriptFunctions
 {
-	final GAME_BOOT:String = "onGameBoot";
+	static public var GAME_BOOT:String = "onGameBoot";
 
 	// State-wise
-	final CREATE:String = "create";
-	final UPDATE:String = "update";
-	final DESTROY:String = "destroy";
-	final CREATE_POST:String = "createPost";
-	final UPDATE_POST:String = "updatePost";
-	final DESTROY_POST:String = "destroyPost";
-	final STEP_HIT:String = "onStepHit";
-	final BEAT_HIT:String = "onBeatHit";
-	final MEASURE_HIT:String = "onMeasureHit";
+	static public var CREATE:String = "create";
+	static public var UPDATE:String = "update";
+	static public var DESTROY:String = "destroy";
+	static public var CREATE_POST:String = "createPost";
+	static public var UPDATE_POST:String = "updatePost";
+	static public var DESTROY_POST:String = "destroyPost";
+	static public var STEP_HIT:String = "onStepHit";
+	static public var BEAT_HIT:String = "onBeatHit";
+	static public var MEASURE_HIT:String = "onMeasureHit";
 
 	// Gameplay-wise
-	final CREATE_STAGE:String = "createStage";
-	final CREATE_STAGE_POST:String = "createStagePost";
-	final GENERATE_SONG:String = "generateSong";
-	final START_COUNTDOWN:String = "startCountdown";
-	final START_SONG:String = "startSong";
-	final END_SONG:String = "endSong";
-	final KEY_DOWN:String = "onKeyDown";
-	final KEY_UP:String = "onKeyUp";
-	final KEY_DOWN_POST:String = "onKeyDownPost";
-	final KEY_UP_POST:String = "onKeyUpPost";
-	final NOTE_HIT:String = "onNoteHit";
-	final NOTE_MISS:String = "onNoteMiss";
-	final NOTE_HIT_POST:String = "onNoteHitPost";
-	final NOTE_MISS_POST:String = "onNoteMissPost";
-	final NOTE_HOLD:String = "onHold";
-	final NOTE_RELEASE:String = "onRelease";
-	final NOTE_HOLD_POST:String = "onHoldPost";
-	final NOTE_RELEASE_POST:String = "onReleasePost";
-	final NEW_NOTE:String = "newNote";
-	final SETUP_NOTE_DATA:String = "setupNoteData";
-	final SETUP_SUSTAIN_DATA:String = "setupSustainData";
-	final SETUP_NOTE_DATA_POST:String = "setupNoteDataPost";
-	final SETUP_SUSTAIN_DATA_POST:String = "setupSustainDataPost";
+	static public var CREATE_STAGE:String = "createStage";
+	static public var CREATE_STAGE_POST:String = "createStagePost";
+	static public var GENERATE_SONG:String = "generateSong";
+	static public var START_COUNTDOWN:String = "startCountdown";
+	static public var START_SONG:String = "startSong";
+	static public var END_SONG:String = "endSong";
+	static public var KEY_DOWN:String = "onKeyDown";
+	static public var KEY_UP:String = "onKeyUp";
+	static public var KEY_DOWN_POST:String = "onKeyDownPost";
+	static public var KEY_UP_POST:String = "onKeyUpPost";
+	static public var NOTE_HIT:String = "onNoteHit";
+	static public var NOTE_MISS:String = "onNoteMiss";
+	static public var NOTE_HIT_POST:String = "onNoteHitPost";
+	static public var NOTE_MISS_POST:String = "onNoteMissPost";
+	static public var NOTE_HOLD:String = "onHold";
+	static public var NOTE_RELEASE:String = "onRelease";
+	static public var NOTE_HOLD_POST:String = "onHoldPost";
+	static public var NOTE_RELEASE_POST:String = "onReleasePost";
+	static public var NEW_NOTE:String = "newNote";
+	static public var SETUP_NOTE_DATA:String = "setupNoteData";
+	static public var SETUP_SUSTAIN_DATA:String = "setupSustainData";
+	static public var SETUP_NOTE_DATA_POST:String = "setupNoteDataPost";
+	static public var SETUP_SUSTAIN_DATA_POST:String = "setupSustainDataPost";
 }
