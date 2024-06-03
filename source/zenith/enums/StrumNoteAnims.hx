76225de348f94f56836f23b58616b920bf6f8dcd
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-06-03 09:06:25 -0400

    Update StrumNoteAnims.hx

--- source/zenith/enums/StrumNoteAnims.hx
+++ source/zenith/enums/StrumNoteAnims.hx
@@ -1,8 +1,8 @@
 package zenith.enums;
 
-enum abstract StrumNoteAnims(String)
+class StrumNoteAnims
 {
-	var HIT = "confirm";
-	var IDLE = "static";
-	var PRESS = "pressed";
+	static public var HIT:String = "confirm";
+	static public var IDLE:String = "static";
+	static public var PRESS:String = "pressed";
 }
