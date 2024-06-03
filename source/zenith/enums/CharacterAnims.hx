c93890eacf1eb7092373db10a1896ab16008af33
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-06-03 08:49:54 -0400

    Update CharacterAnims.hx

--- source/zenith/enums/CharacterAnims.hx
+++ source/zenith/enums/CharacterAnims.hx
@@ -1,17 +1,17 @@
 package zenith.enums;
 
-class CharacterAnims
+enum abstract CharacterAnims(String)
 {
-	static public var IDLE:String = "idle";
-	static public var DL:String = "danceLeft";
-	static public var DR:String = "danceRight";
-	static public var L:String = "singLEFT";
-	static public var D:String = "singDOWN";
-	static public var U:String = "singUP";
-	static public var R:String = "singRIGHT";
-	static public var Lm:String = "singLEFTmiss";
-	static public var Dm:String = "singDOWNmiss";
-	static public var Um:String = "singUPmiss";
-	static public var Rm:String = "singRIGHTmiss";
-	static public var H:String = "hey";
+	var IDLE = "idle";
+	var DL = "danceLeft";
+	var DR = "danceRight";
+	var L = "singLEFT";
+	var D = "singDOWN";
+	var U = "singUP";
+	var R = "singRIGHT";
+	var Lm = "singLEFTmiss";
+	var Dm = "singDOWNmiss";
+	var Um = "singUPmiss";
+	var Rm = "singRIGHTmiss";
+	var H = "hey";
 }
