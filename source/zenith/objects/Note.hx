4b1136092e83c77e4e52afbac0eff9fb08ab34f0
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-06-07 18:01:38 -0400

    Update Note.hx

--- source/zenith/objects/Note.hx
+++ source/zenith/objects/Note.hx
@@ -1,8 +1,3 @@
 package zenith.objects;
 
-class Note extends NoteBase
-{
-	public var sustainLength:Int = 0;
-	public var multiplier:Int = 0;
-	public var state:NoteState = IDLE;
-}
+class Note extends NoteBase { public var sustainLength:Int = 0; public var state:NoteState = IDLE; }
