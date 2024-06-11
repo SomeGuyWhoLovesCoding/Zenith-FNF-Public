75043f38be63b997e27cd2dab6620e439a421832
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-06-11 15:40:42 -0400

    Update NoteBase.hx

--- source/zenith/objects/NoteBase.hx
+++ source/zenith/objects/NoteBase.hx
@@ -2,28 +2,28 @@ package zenith.objects;
 
 class NoteBase extends StaticSprite
 {
-	public var strumTime:Float32 = 0.0;
-	public var noteData:UInt8 = 0;
+	public var strumTime:Float = 0.0;
+	public var noteData:UInt = 0;
 	public var gfNote:Bool = false;
 
-	public var lane:UInt8 = 0;
+	public var lane:UInt = 0;
 
 	public var strum:StrumNote;
 
 	public var targetCharacter:Character;
 
-	public var distance:Float32 = 0.0;
+	public var distance:Float = 0.0;
 
-	public var offsetX:#if cpp cpp.Int16 #else Int #end = 0;
-	public var offsetY:#if cpp cpp.Int16 #else Int #end = 0;
+	public var offsetX:Int = 0;
+	public var offsetY:Int = 0;
 
-	public var direction(default, set):Float32 = 0.0;
+	public var direction(default, set):Float = 0.0;
 
-	function set_direction(value:Float32):Float32
+	function set_direction(value:Float):Float
 	{
 		return direction = value;
 	}
 
 	static public var colorArray:Array<Int> = [0xffc941d5, 0xff00ffff, 0xff0ffb3e, 0xfffa3e3e];
-	static public var angleArray:Array<Float32> = [0.0, -90.0, 90.0, 180.0];
+	static public var angleArray:Array<Float> = [0.0, -90.0, 90.0, 180.0];
 }
